c45e3972689d7a680f6940e571dbcc46a050ad85
Chris Zielinski <ckz8780@gmail.com>
2018-05-31 23:55:27 +0000

    Added call for what's happening in NY, USA

--- twitter_intro.py
+++ twitter_intro.py
@@ -8,3 +8,14 @@ CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
 
 OAUTH_TOKEN = os.getenv('OAUTH_TOKEN')
 OAUTH_TOKEN_SECRET = os.getenv('OAUTH_TOKEN_SECRET')
+
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+NY_WOE_ID = 2459115 # New York, USA
+
+ny_trends = api.trends_place(NY_WOE_ID)
+
+print(json.dumps(ny_trends, indent=1))
