94c4847777825c174e2a6f6be8244881520aaf07
Chris Zielinski <ckz8780@gmail.com>
2018-06-01 01:02:40 +0000

    Added lexical diversity and average word counts to tweet_access.py

--- tweet_access.py
+++ tweet_access.py
@@ -33,3 +33,15 @@ for label, data in (('Text', status_texts), ('Screen Name', screen_names), ('Wor
         [table.add_row(entry) for entry in counter.most_common()[:10]]
         table.align[label], table.align['Count'] = 'l', 'r' # align the columns
         print(table)
+        
+def get_lexical_diversity(items):
+    return 1.0*len(set(items))/len(items)
+    
+def get_average_words(tweets):
+    total_words = sum([len(tweet.split()) for tweet in tweets])
+    return 1.0*total_words/len(tweets)
+    
+print('Average Words: {}'.format(get_average_words(status_texts)))
+print('Word Diversity: {}'.format(get_lexical_diversity(words)))
+print('Screen Name Diversity: {}'.format(get_lexical_diversity(screen_names)))
+print('Hashtag Diversity: {}'.format(get_lexical_diversity(hashtags)))
